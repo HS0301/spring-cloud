8024279733d16caa0f1b2d2f2a321ed98859092c
HS0301 <95467084+HS0301@users.noreply.github.com>
2022-07-28 14:46:58 +0800

    Add files via upload

---
+++ spring_cloud_nacos09/nacos09_01/src/main/java/com/spring/cloud/nacos0901/Nacos09_01.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.nacos0901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther 月鸟石 2022/7/28
+ * 联系方式(QQ)：3054578922
+ * 周四
+ * 欢迎回家，主人
+ */
+@SpringBootApplication
+public class Nacos09_01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nacos09_01.class,args);
+    }
+}
+
+/**
+ * 一切顺心
+ */
---
+++ spring_cloud_nacos09/nacos09_02/src/main/java/com/spring/cloud/nacos0902/Nacos09_02.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.nacos0902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther 月鸟石 2022/7/28
+ * 联系方式(QQ)：3054578922
+ * 周四
+ * 欢迎回家，主人
+ */
+@SpringBootApplication
+public class Nacos09_02 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nacos09_02.class,args);
+    }
+}
+
+/**
+ * 一切顺心
+ */
